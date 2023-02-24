a68dc79421af396643071d3dcd9b04ef8ce33eb0
Adarsh Gopalakrishnan <agkrishnan2003@gmail.com>
2023-02-24 20:24:44 +0530

    Created resources route

---
+++ src/pages/resources/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+
+const ResourcePage = () => {
+    return (
+        <Layout>
+            <div className="content block">
+                <h1 className="title is-1">Resources and Information</h1>
+            </div>
+        </Layout>
+    );
+}
+
+export default ResourcePage;
