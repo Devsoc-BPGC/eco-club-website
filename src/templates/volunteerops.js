2a5102d32a15af0e308e4985216010e9f4f31ca2
Adarsh Gopalakrishnan <agkrishnan2003@gmail.com>
2023-02-24 20:24:19 +0530

    Template for volunteer route

---
+++ src/templates/volunteerops.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { graphql } from "gatsby";
+
+import Layout from '../components/Layout';
+
+const VolunteerPage = () => {
+    return (
+        <Layout>
+            <div>Volunteer Page</div>
+        </Layout>
+    );
+}
+
+export default VolunteerPage;
