3d8c796673a5c44c73b98562f784f2ba9e111f64
Adarsh Gopalakrishnan <agkrishnan2003@gmail.com>
2023-02-27 18:35:05 +0530

    Created template for news page

---
+++ src/templates/news-page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from '../components/Layout';
+
+const NewsPage = () => {
+    return (
+        <Layout>
+            <h1 className="title is-1">News and Updates</h1>
+        </Layout>
+    );
+}
+
+export default NewsPage;
