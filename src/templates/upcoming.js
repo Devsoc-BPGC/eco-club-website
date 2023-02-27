303e733e4568592d8df071e438f42d6ea8dc399e
Adarsh Gopalakrishnan <agkrishnan2003@gmail.com>
2023-02-27 18:35:31 +0530

    Created template for upcoming events page

---
+++ src/templates/upcoming.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from '../components/Layout';
+
+const UpcomingEvents = () => {
+    return (
+        <Layout>
+            <div>Upcoming events here</div>
+        </Layout>
+    );
+}
+
+export default UpcomingEvents;
